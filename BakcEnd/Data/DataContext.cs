f0cd41fc6fd61a7741471ffac34c4f02df451572
Jorge Castillo <J.Castillo@wolterskluwer.com>
2020-10-13 18:53:31 -0500

    inital commit

---
+++ BakcEnd/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using BakcEnd.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BakcEnd.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        { 
+        }
+
+        public DbSet<TarjetaCredito> TarjetaCredito { get; set; }
+    }
+}
